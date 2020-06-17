477dd608dd5f2fec2c462fbfec45d761ec50df5e
Maksim Rezchikov <maxim13rezchikov@gmail.com>
2020-06-17 14:12:22 +0300

    11 Created CrudService interface

--- src/main/java/com/mr13/vacationschedule/components/employee/service/EmployeeService.java
+++ src/main/java/com/mr13/vacationschedule/components/employee/service/EmployeeService.java
@@ -2,18 +2,11 @@ package com.mr13.vacationschedule.components.employee.service;
 
 import com.mr13.vacationschedule.components.employee.domain.Employee;
 import com.mr13.vacationschedule.components.employee.dto.EmployeeForm;
+import com.mr13.vacationschedule.core.service.CrudService;
 
-import java.util.List;
-
-public interface EmployeeService {
+public interface EmployeeService extends CrudService<Employee> {
 
   Employee save(EmployeeForm employeeForm);
 
-  Employee getOne(Long employeeId);
-
   Employee update(Long employeeId, EmployeeForm employeeForm);
-
-  List<Employee> getAll();
-
-  void delete(Long employeeId);
 }
